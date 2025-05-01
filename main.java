06bc93d251a3e6c898aed9dd5e46fa7aa6f03958
Lucas <knightfication@gmail.com>
2025-04-30 21:25:07 -0500

    Created a gitignore since I will start compiling files soon. Also, fixed the Main file.

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
-public static void main (String[] args) throws IOException {
-    int n = 12345; 
-    String shorturl = idToShortURL(n); 
-    System.out.println("Generated short url is " + shorturl); 
-    System.out.println("Id from url is " + 
-                        shortURLtoID(shorturl)); 
+public class Main {
+     public static void Main (String[] args) throws IOException {
+        int n = 12345; 
+        String shorturl = idToShortURL(n); 
+        System.out.println("Generated short url is " + shorturl); 
+        System.out.println("Id from url is " + shortURLtoID(shorturl)); 
+    }
 }
