1f795fdce1b59f5ea9eaf9e7728e6908a3233f30
Lucas <knightfication@gmail.com>
2025-05-01 08:26:22 -0500

    Decode is up. The ShortenerController likely will need more more attention in a bit.

--- java/shortlink/controller/ShortenerController.java
+++ java/shortlink/controller/ShortenerController.java
@@ -1,4 +1,50 @@
+package shortlink.controller;
+
+import com.example.shortlink.exception.TooManyRequestsException;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.shortlink.service.Shortener;
+import com.example.shortlink.dto.UrlResponse;
+import com.example.shortlink.dto.UrlRequest;
+import java.util.concurrent.Semaphore;
+
 @RestController
 public class ShortenerController {
-    
+
+    @Autowired
+    private Shortener shortener;
+
+    @Autowired
+    private Semaphore requestSemaphore;
+
+    @PostMaping ("/encode")
+    public URL Response encode(@RequestBody URLRequest request) {
+        if (!requestSemaphore.tryAcquire()){
+            throw new TooManyRequestsException("Too many concurrent requests");
+        } try {
+            // In a real app, this would go to a database
+            int id = Math.abs(request.getURL().hashCode());
+            String shortURL = "http://short.est/" + shortener.idToShortURL(id); // Likely will need to change this URL, gotta check my parked domains
+            return new URLResponse(shortURL);
+        } finally {
+            requestSemaphore.release();
+        }
+
+        // OK this LOOKS right, but I won't try to compile now. 
+    }
+
+    @PostMaping("/decode")
+    public URLResponse decode (@RequestBody URLRequest request) {
+        if (!requestSemaphore.tryAcquire()){
+            thrown new TooManyRequestsException("Too many concurrent request");
+        } try {
+            String shortCode = request/getURL().replace("http://short.est/", ""); 
+            // Gotta check parked domain and if we can use it, otherwise we can provide a fantasy Link ("You are not alone, take this")
+            int id = shortCode/shortURLToID(shortCode);
+            // No database, so index to look up (isn't that nice for once?)
+            String originalURL = "https://example.com/original-url-from-id-" + id; 
+            // Not sure if this is correct at all? Looks wrong. Let's double check that once we have more of a big picture to connect towards
+        } finally {
+            requestSemaphore.release();
+        }
+    }
 }
