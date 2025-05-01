54daad249e3c303a65521eb303dbf30acb14dba7
Lucas <knightfication@gmail.com>
2025-05-01 03:02:23 -0500

    Modified the original code from GeekForGeeks a lot. A bunch of optimizations. I've put comments in the code for that.

--- Shortener.java
+++ Shortener.java
@@ -1,53 +1,90 @@
 // Java program to generate short url from integer id and 
 // integer id back from short url. 
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 public class Shortener {
     // Comment for nerdy purposes: https://en.wikipedia.org/wiki/Burrows%E2%80%93Wheeler_transform
 
-    // Function to generate a short url from integer ID 
-    public String toShort(int n) 
-    { 
-        // Map to store 62 possible characters 
-        char map[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray(); 
-    
-        StringBuffer shorturl = new StringBuffer(); 
+    /* 
+        Putting these in finals because I don't like magic numbers personally, but also
+        the big final string would perform better as a final if called consistently for 
+        an app that would be doing big N calls for it
+    */
+    private static final String BASE62_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int BASE62_SIZE = 62;
+
+    // I like small names for functions, easier to read and remember
+    public String idToShortURL(int n) 
+    {   
+        // Empty check
+        if (n == 0) {
+            return "";
+        }
+
+        /*        
+            StringBuilder instead of StringBuffer since it would perform better for consistent string permutations.
+            While StringBuffer is thread-safe, this class wouldn't be a problem for threading, since you would have to instatiate it 
+            per call, and not trying to change the String on the fly on the same thread.
+        */
+
+        StringBuilder sb = new StringBuilder(); 
     
         // Convert given integer id to a base 62 number 
-        while (n > 0) 
-        { 
-            // use above map to store actual character 
-            // in short url 
-            shorturl.append(map[n % 62]);
-            n = n / 62; 
-        } 
-    
-        // Reverse shortURL to complete base conversion 
-        return shorturl.reverse().toString(); 
+        while (n > 0) {
+            int remainder = n % BASE62_SIZE;
+            sb.append(BASE62_CHARS.charAt(remainder));
+            // Appending characters in reverse order by parsing the remainder to remove some unnecessary reverse() calls
+            n /= BASE62_SIZE;
+        }
+        return sb.reverse().toString();
     } 
     
     // Function to get integer ID back from a short url 
-    public int toID(String shortURL) 
-    { 
-        int id = 0; // initialize result 
-    
-        // A simple base conversion logic 
-        for (int i = 0; i < shortURL.length(); i++) 
-        { 
-            if ('a' <= shortURL.charAt(i) && 
-                       shortURL.charAt(i) <= 'z') 
-            id = id * 62 + shortURL.charAt(i) - 'a'; 
-            if ('A' <= shortURL.charAt(i) && 
-                       shortURL.charAt(i) <= 'Z') 
-            id = id * 62 + shortURL.charAt(i) - 'A' + 26; 
-            if ('0' <= shortURL.charAt(i) && 
-                       shortURL.charAt(i) <= '9') 
-            id = id * 62 + shortURL.charAt(i) - '0' + 52; 
-        } 
-        return id; 
-    } 
-}
+    public int shortURLToID(String sb) {
+        if (sb == null || sb.isEmpty()) {
+            return 0;
+        }
 
-// This code is contributed by shubham96301
+        int id = 0;
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            int value = -1;
+
+            // Find the index of the character in BASE62_CHARS
+            if (c >= 'a' && c <= 'z') {
+                value = c - 'a';
+            } else if (c >= 'A' && c <= 'Z') {
+                value = 26 + (c - 'A');
+            } else if (c >= '0' && c <= '9') {
+                value = 52 + (c - '0');
+            }
+
+            if (value == -1) { // Invalid character
+                return -1; // Return error, for the sake of this exercise, I'll just leave it as a -1
+            }
+
+            id = id * BASE62_SIZE + value;
+        }
+        return id;
+    }
+
+    public String publishErrors() {
+        int statusCode = shortURLToID("");
+        
+        switch (statusCode) {
+            case 0:
+                return "This link has no relation in our database. You could make one right now. <3 ";
+                
+            case -1:
+                return "A character in your link is invalid. For a list of valid characters, click here.";
+                
+            default:
+                // Handle other status codes if necessary, for future proofing (that will never be used, but you know)
+                return "An unexpected error occurred with status code: " + statusCode;
+        }
+    }
+
+    public void reference() {
+        System.out.println ("See ya, Space Cowboy");
+    }
+}
